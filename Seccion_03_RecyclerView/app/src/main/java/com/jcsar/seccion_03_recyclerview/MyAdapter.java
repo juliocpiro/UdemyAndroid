3cb3cff5967dd48be6669ced41ef10c800329bdc
PE\NP000216 <jpiro@gmd.com.pe>
2017-10-04 16:38:48 -0500

    items

---
+++ Seccion_03_RecyclerView/app/src/main/java/com/jcsar/seccion_03_recyclerview/MyAdapter.java
@@ -0,0 +1,60 @@
+package com.jcsar.seccion_03_recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<String> names;
+    private int layout;
+    private OnItemClickListener itemClickListener;
+
+    public MyAdapter(List<String> names, int layout, OnItemClickListener itemClickListener) {
+        this.names = names;
+        this.layout = layout;
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(layout,parent,false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bind(names.get(position), itemClickListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.names.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView itemTexView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.itemTexView = (TextView) itemView.findViewById(R.id.textViewName);
+        }
+        public void bind(final String name, final OnItemClickListener listener){
+            this.itemTexView.setText(name);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.OnItemClick(name,getAdapterPosition());
+                }
+            });
+        }
+    }
+
+    private interface OnItemClickListener{
+        public void OnItemClick(String name, int position);
+    }
+}
